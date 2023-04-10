c0e8797a96533a628355f79fcbb9a63359cf9045
Larissa Amadeu <104395388+Larissa-Amadeu@users.noreply.github.com>
2023-04-09 23:53:30 -0300

    Add files via upload

---
+++ database/migrations/2023_04_10_024709_create_formularios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formularios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomeProjeto');
+            $table->string('desktop');
+            $table->string('descricao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formularios');
+    }
+};
