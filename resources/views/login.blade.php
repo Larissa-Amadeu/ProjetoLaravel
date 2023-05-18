ed3168ca28022e3a90038e1ff1efd926aa53da99
Larissa Amadeu <104395388+Larissa-Amadeu@users.noreply.github.com>
2023-05-18 18:12:31 -0300

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -60,8 +60,8 @@
             @if(Session::has('succes'))
                   <div class="alert alert-success">{{Session::get('success')}}</div>
                   @endif
-                  @if(Session::has('fail'))
-                  <div class="alert alert-danger">{{Session::get('success')}}</div>
+                  @if(Session::has('fail'))               
+                  <div class="alert alert-danger">{{Session::get('fail')}}</div>
                   @endif
               @csrf
               <div class="form-floating mb-3">
